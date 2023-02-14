44402ce99025c29a44d6ceb7d5ae36546bc496cd
Davyd.AR <9766288@mail.ru>
2023-02-14 20:56:26 +0300

    Finish task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -6,13 +6,18 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10-99: {number}");
 
-int maxDigit=MaxDigit(number);
+int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра числа - {maxDigit}");
 
 int MaxDigit(int num)
 {
     int firstDigit = num / 10;
     int secondDigit = num % 10;
-    if (firstDigit > secondDigit) return firstDigit;
-    else return secondDigit;
+    // if (firstDigit > secondDigit) return firstDigit;
+    // return secondDigit; //else не обязательно, т.к. return делает выход из метода
+
+    // int result firstDigit > secondDigit ? firstDigit : secondDigit;
+    // return result;
+
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
 }

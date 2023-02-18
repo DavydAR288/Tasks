f78278413ccded1f8062ad503e807c8de770c061
Davyd.AR <9766288@mail.ru>
2023-02-18 12:54:42 +0300

    correct task19 - method bool

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,27 +6,36 @@
 
 int number = Prompt("Введите натуральное число: ");
 
-if (number > 0) Palindrome(number); //проверка числа на натуральность
+if (number > 0) //проверка числа на натуральность
+{
+    Output(Palindrome(number), number);
+}
 else Console.WriteLine("Введено не натуральное число. Начните заново.");
 
-void Palindrome(int num)    //невозвратный метод, вывод результата в консоль
+bool Palindrome(int num)    //метод проверки палиндрома, возвращает истина/ложь
 {
-    int numX=0;
-    int numOriginal=num;
-    while (num>0)   //разворот числа
+    int numX = 0;
+    int numOriginal = num;
+    while (num > 0)   //разворот числа
     {
-        numX=numX*10+num%10;
-        num/=10;
+        numX = numX * 10 + num % 10;
+        num /= 10;
     }
 
-    if (numOriginal==numX) Console.WriteLine($"Число {numOriginal} является палиндромом");
-    else Console.WriteLine($"Число {numOriginal} НЕ является палиндромом");
+    if (numOriginal == numX) return true;
+    else return false;
 }
 
-int Prompt(string message)  //метод ввода
+int Prompt(string message)  //метод ввода, возвращает число
 {
     Console.Write(message);
     int cc = Convert.ToInt32(Console.ReadLine());
     return cc;
+}
+
+void Output (bool inputRes, int numOriginal) //невозвратный метод вывода в консоль, получает истину/ложь и число
+{
+    if (inputRes) Console.WriteLine($"Число {numOriginal} является палиндромом");
+    else Console.WriteLine($"Число {numOriginal} НЕ является палиндромом");
 }
 

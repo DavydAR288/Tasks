7b70ab0a43a73184057f741cd71fa23e34587a67
Davyd.AR <9766288@mail.ru>
2023-02-24 13:25:48 +0300

    finish tasks 34, 36, 38

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,3 +1,83 @@
 ﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int quantity = Prompt("Введите число элементов массива (натуральное): ", 2);
+int min = -99; //Prompt("Укажите минимальный элемент массива (любое): ", 0);
+int max = 99; //Prompt("Укажите максимальный элемент массива (любое): ", 0);
+int[] array = new int[quantity];
+array = CreateArray(array, min, max);
+
+Console.Write("Массив -> ");
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
+
+Console.Write($"Сумма элементов на нечетных позициях -> {CheckArray(array)}");
+
+int Prompt(string message, int checkNum) // checkNum: -1 отрицательные; 0 любые; 1 положительные; 2 натуральные;
+{
+    int k = 0; 
+    while (k == 0)
+    {
+        Console.Write(message);
+        int cc = Convert.ToInt32(Console.ReadLine());
+
+        if (checkNum == 0) // любые
+        {
+            return cc;
+        }
+
+        if (cc >= 0 && checkNum == 1) // положительные
+        {
+            return cc;
+        }
+
+        if (cc > 0 && checkNum == 2) // натуральные
+        {
+            return cc;
+        }
+
+        if (cc <= 0 && checkNum == -1) // отрицательные
+        {
+            return cc;
+        }
+
+        Console.WriteLine("Число не соответствует заданному диапазону. Повторите ввод.");
+    }
+    return 0;
+}
+
+int[] CreateArray(int[] arr, int min, int max)
+{
+    int count = arr.Length;
+    Random rnd = new Random(); //создает псевдослучайное число по системному времени 15мс
+    for (int i = 0; i < count; i++)
+    {
+        arr[i]=rnd.Next(min,max+1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        if (i<arr.Length-1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int CheckArray(int[] arr) // суммирование элементов с четными индексами (нечетными местами при выводе)
+{
+    int sum=0;
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        if (i%2==0) sum+=arr[i];
+    }
+    return sum;
+}
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,84 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+int quantity = Prompt("Введите число элементов массива (натуральное): ", 2);
+int min = 0; //Prompt("Укажите минимальный элемент массива (любое): ", 0);
+int max = 99; //Prompt("Укажите максимальный элемент массива (любое): ", 0);
+double[] array = new double[quantity];
+array = CreateArray(array, min, max);
+
+Console.Write("Массив -> ");
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
+
+Console.Write($"Разница между максимальным и минимальным элементами массива -> {CheckArray(array)}");
+
+int Prompt(string message, int checkNum) // checkNum: -1 отрицательные; 0 любые; 1 положительные; 2 натуральные;
+{
+    int k = 0; 
+    while (k == 0)
+    {
+        Console.Write(message);
+        int cc = Convert.ToInt32(Console.ReadLine());
+
+        if (checkNum == 0) // любые
+        {
+            return cc;
+        }
+
+        if (cc >= 0 && checkNum == 1) // положительные
+        {
+            return cc;
+        }
+
+        if (cc > 0 && checkNum == 2) // натуральные
+        {
+            return cc;
+        }
+
+        if (cc <= 0 && checkNum == -1) // отрицательные
+        {
+            return cc;
+        }
+
+        Console.WriteLine("Число не соответствует заданному диапазону. Повторите ввод.");
+    }
+    return 0;
+}
+
+double[] CreateArray(double[] arr, int min, int max)
+{
+    int count = arr.Length;
+    Random rnd = new Random(); //создает псевдослучайное число по системному времени 15мс
+    for (int i = 0; i < count; i++)
+    {
+        arr[i]=rnd.Next(min,max+1);
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        if (i<arr.Length-1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+double CheckArray(double[] arr) // суммирование элементов с четными индексами (нечетными местами при выводе)
+{
+    double sum=0, max=arr[0], min=arr[0];
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        if (arr[i]>max) max=arr[i];
+        if (arr[i]<min) min=arr[i];
+    }
+    sum=max-min;
+    return sum;
+}

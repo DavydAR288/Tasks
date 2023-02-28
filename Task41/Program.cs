8795df988502e8a716d3905458e906eec6e11da7
Davyd.AR <9766288@mail.ru>
2023-02-28 23:41:16 +0300

    Finish task 41, 43

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = (Console.ReadLine());
+    int result = Convert.ToInt32(value);
+    Console.WriteLine();
+    return result;
+}
+
+int[] CreateArray (int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите число №{i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int SumArray (int[] arr)
+{
+    int[] arrCopy = new int[arr.Length];
+    int count=0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>0) count+=1;
+    }
+    return count;
+}
+
+
+
+Console.Clear();
+int num = Promt("Введите число M: ");
+if (num <= 0) Console.WriteLine("Введено некорректное число!");
+else
+{
+    int[] array = CreateArray(num);
+    int sum = SumArray(array);
+    PrintArray(array);
+    Console.Write($"; введено положительных чисел -> {sum} ");
+}
--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Promt(string message)
+{
+    Console.Write(message);
+    string value = (Console.ReadLine());
+    double result = Convert.ToInt32(value);
+    // Console.WriteLine();
+    return result;
+}
+
+double[] FindXY(double b1, double k1, double b2, double k2)
+{
+    double[] arr = new double[2];
+    arr[0] = (b2 - b1) / (k1 - k2); 
+    arr[1] = (b1*k2-b2*k1)/(k2-k1);
+    return arr;
+}
+
+void PrintXY(double[] arrayXY)
+{
+    var roundX = Math.Round(arrayXY[0], 1, MidpointRounding.ToZero); 
+    var roundY = Math.Round(arrayXY[1], 1, MidpointRounding.ToZero);
+    Console.Write($"Координаты точки пересечения двух прямых: ({roundX}; {roundY})");
+}
+
+
+
+Console.Clear();
+Console.WriteLine($"Введите b1, k1, b2 и k2 для уравнений y = k1 * x + b1, y = k2 * x + b2");
+double numB1 = Promt("b1 = ");
+double numK1 = Promt("k1 = ");
+double numB2 = Promt("b2 = ");
+double numK2 = Promt("k2 = ");
+double[] arrayResult = FindXY(numB1, numK1, numB2, numK2);
+PrintXY(arrayResult);
+
+

5ca6ed8db629f86b88a63a683e3c8bb99a1027e5
Davyd.AR <9766288@mail.ru>
2023-03-08 02:29:40 +0300

    finish task 54. in work task 56

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -7,3 +7,73 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6} "); // форматирование строки
+        }
+        Console.WriteLine();
+    }
+}
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = (Console.ReadLine());
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+
+void SortMatrix(int[,] matrix)
+{
+    int temp;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 1 + j; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, j])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+    return;
+}
+
+
+Console.Clear();
+
+int matrixRows = 3; //Promt("Введите количество строк: ");
+int matrixColumns = 6; //Promt("Введите количество столбцов: ");
+int maxRnd = 10; //Promt("Введите верхний предел случйного числа: ");
+int minRnd = -10; //Promt("Введите нижний предел случйного числа: ");
+
+int[,] array2D = CreateMatrixRndInt(matrixRows, matrixColumns, minRnd, maxRnd);
+PrintArray(array2D);
+Console.WriteLine();
+SortMatrix(array2D);
+PrintArray(array2D);
--- Task56/Program.cs
+++ Task56/Program.cs
@@ -6,3 +6,69 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6} "); // форматирование строки
+        }
+        Console.WriteLine();
+    }
+}
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = (Console.ReadLine());
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+
+int[] FindRowSum(int[,] matrix)
+{
+    int sum = 0, row = 0, minSum=0;
+    int[] sumMatrix = new int[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        sumMatrix[i]=sum;
+        sum=0;
+    }
+    return sumMatrix;
+}
+
+
+
+Console.Clear();
+
+int matrixRows = 3; //Promt("Введите количество строк: ");
+int matrixColumns = 6; //Promt("Введите количество столбцов: ");
+int maxRnd = 10; //Promt("Введите верхний предел случйного числа: ");
+int minRnd = 1; //Promt("Введите нижний предел случйного числа: ");
+
+int[,] array2D = CreateMatrixRndInt(matrixRows, matrixColumns, minRnd, maxRnd);
+PrintArray(array2D);
+Console.WriteLine();
+int[] result = FindRowSum(array2D);
+Console.WriteLine(result);
+

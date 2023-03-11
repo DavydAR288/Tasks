34462198e7fbffba7bde26232bbf3de9ce94cf75
Davyd.AR <9766288@mail.ru>
2023-03-11 18:48:57 +0300

    finish task 64

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,3 +1,17 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int SumRec(int num1, int num2)
+{
+    if (num2 == num1) return 0;
+    else return num2 + SumRec(num2 - 1, num1);
+}
+
+
+Console.Write("Введите число 1: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumRec(numberOne, numberTwo));

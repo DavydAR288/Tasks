db666bece72f045f93cf202014ea7637009aad1e
Davyd.AR <9766288@mail.ru>
2023-03-11 18:46:14 +0300

    finish task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+String Recursion(int num)
+{
+    if (num == 0) return string.Empty;
+    return num + " " + Recursion(num - 1);
+}
+
+Console.Write("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number>0) Console.Write(Recursion(number));
+else Console.Write("Ошибка ввода");
---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+//
+// A(m, n)  = n + 1, если m = 0,
+//          = A(m - 1, 1), если m > 0, n = 0,
+//          = A(m - 1, A(m, n - 1)), если m > 0, n > 0.
+
+public class Solution {
+    public static int recursion(int m, int n) {
+        // Базовый случай
+        if (m == 0) {
+            return n + 1;
+        } // Шаг рекурсии / рекурсивное условие
+        else if (n == 0 && m > 0) {
+            return recursion(m - 1, 1);
+        } // Шаг рекурсии / рекурсивное условие
+        else {
+            return recursion(m - 1, recursion(m, n - 1));
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(recursion(3, 2)); // вызов рекурсивной функции
+    }
+}
